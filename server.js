9335046cd8cdd93f6d56a806b0f203b3de572cb1
Matt <matthewjmcivor@gmail.com>
2020-09-14 19:39:18 -0500

    Added Dotenv

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
+require('dotenv').config();
 const express = require("express");
-const exphbs = require("express-handlebars");
+// const exphbs = require("express-handlebars");
 const cors = require('cors');
 const bodyParser = require("body-parser");
 const app = express();
